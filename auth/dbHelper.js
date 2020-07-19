c57c4e1367e1d19854b50fd49d80d6efcf128274
procozo <procodezone@gmail.com>
2020-07-19 18:01:09 +0530

    first commit with full code

--- auth/dbHelper.js
+++ auth/dbHelper.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 module.exports = {
     checkDBStatus : () =>{
         mongoose.connect(
-            "mongodb+srv://hrcloud:hr@123@cluster0.icwl5.mongodb.net/hrcloud?retryWrites=true&w=majority",
+            "mongodb+srv://hrcloud:<password>@cluster0.icwl5.mongodb.net/hrcloud?retryWrites=true&w=majority",
             { useNewUrlParser: true, useUnifiedTopology: true },
             function (err, db) {
               if (err) return false
