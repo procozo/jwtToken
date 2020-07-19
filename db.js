240f9c541550b77f3802df67d7bc825a5b78c7d6
procozo <procodezone@gmail.com>
2020-07-19 17:59:11 +0530

    first commit with full code

--- db.js
+++ db.js
@@ -1,11 +1,4 @@
 var mongoose = require('mongoose');
 // mongoose.set()
-// mongoose.connect('mongodb+srv://hrcloud:hr@123@cluster0.icwl5.mongodb.net/hrcloud?retryWrites=true&w=majority', {useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://hrcloud:<password>@cluster0.icwl5.mongodb.net/hrcloud?retryWrites=true&w=majority', {useNewUrlParser:true,useUnifiedTopology:true});
 // // mongoose.isValidObjectId./
-
-// mongoose.connect('mongodb+srv://disha:disha@123@cluster0.icwl5.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser:true,useUnifiedTopology:true},function(err,client){
-//     if(err)
-//     console.log(err)
-// console.log("connected",client)
-// });
-mongoose.connect('mongodb+srv://disha:disha@123@cluster0.icwl5.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser:true,useUnifiedTopology:true});
